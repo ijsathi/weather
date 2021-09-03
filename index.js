cd0391c0888174de46bb795d02c44ae2f3b910a3
ijsathi <ijsathi2001rs@gmail.com>
2021-09-03 11:51:05 +0600

    weather

---
+++ index.js
@@ -0,0 +1,21 @@
+const API_KEY = `6be112ba03cb2bfc955b0b846ff35687`;
+const searchTemperature = () =>{
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+const setInnerText = (id, Text)=>{
+    document.getElementById(id).innerText = Text;
+}
+const displayTemperature = temperature =>{
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    // set weather icon
+    const url = ` http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url)
+    // console.log(temperature);
+}
